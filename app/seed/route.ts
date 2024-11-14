2a2cd41409b766d3cc5d04704e5c8c27fe39fc3b
Milan Snoeijink <snoeijinkmilan@gmail.com>
2024-11-14 11:28:41 +0100

    database?

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -7,22 +7,22 @@ const client = await db.connect();
 async function seedUsers() {
   await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
   await client.sql`
-     CREATE TABLE IF NOT EXISTS users (
-       id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-       name VARCHAR(255) NOT NULL,
-       email TEXT NOT NULL UNIQUE,
-       password TEXT NOT NULL
-     );
-   `;
+    CREATE TABLE IF NOT EXISTS users (
+      id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+      name VARCHAR(255) NOT NULL,
+      email TEXT NOT NULL UNIQUE,
+      password TEXT NOT NULL
+    );
+  `;
 
   const insertedUsers = await Promise.all(
     users.map(async (user) => {
       const hashedPassword = await bcrypt.hash(user.password, 10);
       return client.sql`
-           INSERT INTO users (id, name, email, password)
-           VALUES (${user.id}, ${user.name}, ${user.email}, ${hashedPassword})
-          ON CONFLICT (id) DO NOTHING;
-         `;
+        INSERT INTO users (id, name, email, password)
+        VALUES (${user.id}, ${user.name}, ${user.email}, ${hashedPassword})
+        ON CONFLICT (id) DO NOTHING;
+      `;
     }),
   );
 
@@ -33,22 +33,22 @@ async function seedInvoices() {
   await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
 
   await client.sql`
-       CREATE TABLE IF NOT EXISTS invoices (
-         id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-         customer_id UUID NOT NULL,
-         amount INT NOT NULL,
-         status VARCHAR(255) NOT NULL,
-         date DATE NOT NULL
-       );
-     `;
+    CREATE TABLE IF NOT EXISTS invoices (
+      id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+      customer_id UUID NOT NULL,
+      amount INT NOT NULL,
+      status VARCHAR(255) NOT NULL,
+      date DATE NOT NULL
+    );
+  `;
 
   const insertedInvoices = await Promise.all(
     invoices.map(
       (invoice) => client.sql`
-           INSERT INTO invoices (customer_id, amount, status, date)
-           VALUES (${invoice.customer_id}, ${invoice.amount}, ${invoice.status}, ${invoice.date})
-           ON CONFLICT (id) DO NOTHING;
-         `,
+        INSERT INTO invoices (customer_id, amount, status, date)
+        VALUES (${invoice.customer_id}, ${invoice.amount}, ${invoice.status}, ${invoice.date})
+        ON CONFLICT (id) DO NOTHING;
+      `,
     ),
   );
 
@@ -59,21 +59,21 @@ async function seedCustomers() {
   await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
 
   await client.sql`
-       CREATE TABLE IF NOT EXISTS customers (
-         id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-         name VARCHAR(255) NOT NULL,
-         email VARCHAR(255) NOT NULL,
-         image_url VARCHAR(255) NOT NULL
-       );
-     `;
+    CREATE TABLE IF NOT EXISTS customers (
+      id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+      name VARCHAR(255) NOT NULL,
+      email VARCHAR(255) NOT NULL,
+      image_url VARCHAR(255) NOT NULL
+    );
+  `;
 
   const insertedCustomers = await Promise.all(
     customers.map(
       (customer) => client.sql`
-           INSERT INTO customers (id, name, email, image_url)
-           VALUES (${customer.id}, ${customer.name}, ${customer.email}, ${customer.image_url})
-           ON CONFLICT (id) DO NOTHING;
-         `,
+        INSERT INTO customers (id, name, email, image_url)
+        VALUES (${customer.id}, ${customer.name}, ${customer.email}, ${customer.image_url})
+        ON CONFLICT (id) DO NOTHING;
+      `,
     ),
   );
 
@@ -82,19 +82,19 @@ async function seedCustomers() {
 
 async function seedRevenue() {
   await client.sql`
-       CREATE TABLE IF NOT EXISTS revenue (
-         month VARCHAR(4) NOT NULL UNIQUE,
-         revenue INT NOT NULL
-       );
-     `;
+    CREATE TABLE IF NOT EXISTS revenue (
+      month VARCHAR(4) NOT NULL UNIQUE,
+      revenue INT NOT NULL
+    );
+  `;
 
   const insertedRevenue = await Promise.all(
     revenue.map(
       (rev) => client.sql`
-           INSERT INTO revenue (month, revenue)
-           VALUES (${rev.month}, ${rev.revenue})
-           ON CONFLICT (month) DO NOTHING;
-         `,
+        INSERT INTO revenue (month, revenue)
+        VALUES (${rev.month}, ${rev.revenue})
+        ON CONFLICT (month) DO NOTHING;
+      `,
     ),
   );
 
